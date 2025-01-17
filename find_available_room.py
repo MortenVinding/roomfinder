e44f97ff9cd1aaa36cd2457ae5a1be3522841ee3
Morten Vinding <mvi@dmi.dk>
2025-01-17 08:41:37 +0100

    Convert to python3

--- find_available_room.py
+++ find_available_room.py
@@ -1,15 +1,11 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
-import sys
-reload(sys)
-sys.setdefaultencoding("utf-8")
 
 import subprocess
 import getpass
 from string import Template
 import xml.etree.ElementTree as ET
-import csv, codecs
+import csv
 import argparse
 import datetime
 
@@ -18,50 +14,54 @@ starttime_default = now.isoformat()
 end_time_default = None
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-url","--url", help="url for exhange server, e.g. 'https://mail.domain.com/ews/exchange.asmx'.",required=True)
-parser.add_argument("-u","--user", help="user name for exchange/outlook",required=True)
-parser.add_argument("-start","--starttime", help="Starttime e.g. 2014-07-02T11:00:00 (default = now)", default=starttime_default)
-parser.add_argument("-end","--endtime", help="Endtime e.g. 2014-07-02T12:00:00 (default = now+1h)", default=end_time_default)
+parser.add_argument("-url", "--url", help="URL for Exchange server, e.g. 'https://mail.domain.com/ews/exchange.asmx'.", required=True)
+parser.add_argument("-u", "--user", help="User name for Exchange/Outlook", required=True)
+parser.add_argument("-start", "--starttime", help="Starttime e.g. 2014-07-02T11:00:00 (default = now)", default=starttime_default)
+parser.add_argument("-end", "--endtime", help="Endtime e.g. 2014-07-02T12:00:00 (default = now+1h)", default=end_time_default)
 #parser.add_argument("-n","--now", help="Will set starttime to now and endtime to now+1h", action="store_true")
-parser.add_argument("-f","--file", help="csv filename with rooms to check (default=rooms.csv). Format: Name,email",default="rooms.csv")
+parser.add_argument("-f", "--file", help="CSV filename with rooms to check (default=rooms.csv). Format: Name,email", default="rooms.csv")
 
-args=parser.parse_args()
+args = parser.parse_args()
 
 url = args.url
 
-rooms={}
-reader = csv.reader(codecs.open(args.file, 'r', encoding='utf-8')) 
-for row in reader: 
-	rooms[unicode(row[1])]=unicode(row[0])
+rooms = {}
+with open(args.file, 'r', encoding='utf-8') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        rooms[row[1]] = row[0]
 
 start_time = args.starttime
 if not args.endtime:
-	start = datetime.datetime.strptime( start_time, "%Y-%m-%dT%H:%M:%S" )
-	end_time = (start + datetime.timedelta(hours=1)).isoformat()
+    start = datetime.datetime.strptime(start_time, "%Y-%m-%dT%H:%M:%S")
+    end_time = (start + datetime.timedelta(hours=1)).isoformat()
 else:
-	end_time = args.endtime
+    end_time = args.endtime
 
 user = args.user
 password = getpass.getpass("Password:")
 
-print "Searching for a room from " + start_time + " to " + end_time + ":"
-print "{0:10s} {1:64s} {2:64s}".format("Status", "Room", "Email")
+print(f"Searching for a room from {start_time} to {end_time}:")
+print(f"{'Status':<10} {'Room':<64} {'Email':<64}")
 
-xml_template = open("getavailibility_template.xml", "r").read()
+with open("getavailibility_template.xml", "r", encoding='utf-8') as template_file:
+    xml_template = template_file.read()
+
 xml = Template(xml_template)
 for room in rooms:
-	data = unicode(xml.substitute(email=room,starttime=start_time,endtime=end_time))
+    data = xml.substitute(email=room, starttime=start_time, endtime=end_time)
 
-	header = "\"content-type: text/xml;charset=utf-8\""
-	command = "curl --silent --header " + header +" --data '" + data + "' --ntlm "+"--negotiate "+ "-u "+ user+":"+password+" "+ url
-	response = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True).communicate()[0]
+    header = "\"content-type: text/xml;charset=utf-8\""
+    command = f"curl --silent --header {header} --data '{data}' --ntlm --negotiate -u '{user}:{password}' {url}"
+    response = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True).communicate()[0]
 
-	tree = ET.fromstring(response)
+    tree = ET.fromstring(response)
 
-	status = "Free"
-	# arrgh, namespaces!!
-	elems=tree.findall(".//{http://schemas.microsoft.com/exchange/services/2006/types}BusyType")
-	for elem in elems:
-		status=elem.text
+    status = "Free"
+    # Namespaces are tricky with XML, but this works for the Exchange format
+    elems = tree.findall(".//{http://schemas.microsoft.com/exchange/services/2006/types}BusyType")
+    for elem in elems:
+        status = elem.text
 
-	print "{0:10s} {1:64s} {2:64s}".format(status, rooms[room], room)
+    print(f"{status:<10} {rooms[room]:<64} {room:<64}")
+
--- find_rooms.py
+++ find_rooms.py
@@ -1,67 +1,65 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 import sys
-reload(sys)
-sys.setdefaultencoding("utf-8")
-
-from string import Template
-from string import letters
-from string import digits
 import subprocess
 import getpass
 import xml.etree.ElementTree as ET
 import argparse
 import csv
 import operator
+from string import Template
+import string
 
 def findRooms(prefix):
-	rooms={}
-	data = unicode(xml.substitute(name=prefix))
+    rooms = {}
+    data = xml.substitute(name=prefix)
 
-	header = "\"content-type: text/xml;charset=utf-8\""
-	command = "curl --silent --header " + header +" --data '" + data + "' --ntlm "+"--negotiate "+ "-u "+ user+":"+password+" "+ url
-	response = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True).communicate()[0]
-	tree = ET.fromstring(response)
+    header = "\"content-type: text/xml;charset=utf-8\""
+    command = f"curl --silent --header {header} --data '{data}' --ntlm --negotiate -u '{user}:{password}' {url}"
+    response = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True).communicate()[0]
+    print(f"XML return: {data}")
+    tree = ET.fromstring(response)
 
-	elems=tree.findall(".//{http://schemas.microsoft.com/exchange/services/2006/types}Resolution")
-	for elem in elems:
-		email = elem.findall(".//{http://schemas.microsoft.com/exchange/services/2006/types}EmailAddress")
-		name = elem.findall(".//{http://schemas.microsoft.com/exchange/services/2006/types}DisplayName")
-		if len(email) > 0 and len(name) > 0:
-			rooms[email[0].text] = name[0].text
-	return rooms		
+    elems = tree.findall(".//{http://schemas.microsoft.com/exchange/services/2006/types}Resolution")
+    for elem in elems:
+        email = elem.findall(".//{http://schemas.microsoft.com/exchange/services/2006/types}EmailAddress")
+        name = elem.findall(".//{http://schemas.microsoft.com/exchange/services/2006/types}DisplayName")
+        if len(email) > 0 and len(name) > 0:
+            rooms[email[0].text] = name[0].text
+    return rooms
 
 parser = argparse.ArgumentParser()
-parser.add_argument("prefix", nargs='+',help="A list of prefixes to search for. E.g. 'conference confi'")
-parser.add_argument("-url","--url", help="url for exhange server, e.g. 'https://mail.domain.com/ews/exchange.asmx'.",required=True)
-parser.add_argument("-u","--user", help="user name for exchange/outlook", required=True)
-parser.add_argument("-d","--deep", help="Attemp a deep search (takes longer).", action="store_true")
-args=parser.parse_args()
+parser.add_argument("prefix", nargs='+', help="A list of prefixes to search for. E.g. 'conference confi'")
+parser.add_argument("-url", "--url", help="URL for Exchange server, e.g. 'https://mail.domain.com/ews/exchange.asmx'.", required=True)
+parser.add_argument("-u", "--user", help="User name for Exchange/Outlook", required=True)
+parser.add_argument("-d", "--deep", help="Attempt a deep search (takes longer).", action="store_true")
+args = parser.parse_args()
 
 url = args.url
 user = args.user
 password = getpass.getpass("Password:")
 
-xml_template = open("resolvenames_template.xml", "r").read()
+with open("resolvenames_template.xml", "r", encoding='utf-8') as template_file:
+    xml_template = template_file.read()
 xml = Template(xml_template)
 
-rooms={}
+rooms = {}
 
 for prefix in args.prefix:
-	rooms.update(findRooms(prefix))
-	print "After searching for prefix '" + prefix + "' we found " + str(len(rooms)) + " rooms."
+    rooms.update(findRooms(prefix))
+    print(f"After searching for prefix '{prefix}' we found {len(rooms)} rooms.")
 
-	deep = args.deep
+    if args.deep: 
+        symbols = string.ascii_letters + string.digits
+        for symbol in symbols:
+            prefix_deep = f"{prefix} {symbol}"
+            rooms.update(findRooms(prefix_deep))
 
-	if deep: 
-		symbols = letters + digits
-		for symbol in symbols:
-			prefix_deep = prefix + " " + symbol
-			rooms.update(findRooms(prefix_deep))
+        print(f"After deep search for prefix '{prefix}' we found {len(rooms)} rooms.")
 
-		print "After deep search for prefix '" + prefix + "' we found " + str(len(rooms)) + " rooms."
-
-writer = csv.writer(open("rooms.csv", "wb"))
-for item in sorted(rooms.iteritems(), key=operator.itemgetter(1)):
-	writer.writerow([item[1],item[0]])
+# Writing to CSV file
+with open("rooms.csv", "w", newline='', encoding='utf-8') as csvfile:
+    writer = csv.writer(csvfile)
+    for item in sorted(rooms.items(), key=operator.itemgetter(1)):
+        writer.writerow([item[1], item[0]])
